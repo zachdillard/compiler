33fd08bb3641588e20e2838c861340320ff969a3
Zach Dillard <zachdillard@users.noreply.github.com>
2026-07-18 17:56:21 -0400

    Read and print a C file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+if (args.Length != 1)
+{
+  Console.Error.WriteLine("Usage: Compiler <source-file>");
+  return 1;
+}
+
+try
+{
+  var contents = File.ReadAllText(args[0], Encoding.UTF8);
+  Console.Write(contents);
+  return 0;
+}
+catch (IOException)
+{
+  Console.Error.WriteLine($"Error: could not read source file '{args[0]}'.");
+  return 1;
+}
+catch (UnauthorizedAccessException)
+{
+  Console.Error.WriteLine($"Error: could not read source file '{args[0]}'.");
+  return 1;
+}

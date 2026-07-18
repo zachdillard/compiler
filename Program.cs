020cb4dbd53cb2b6f3107d3454592173978783f5
Zach Dillard <zachdillard@users.noreply.github.com>
2026-07-18 18:08:21 -0400

    Preprocess attempt

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 
 if (args.Length != 1)
 {
@@ -6,19 +7,40 @@ if (args.Length != 1)
   return 1;
 }
 
+var inputFile = args[0];
+var preprocessedFile = Path.ChangeExtension(inputFile, ".i");
+var startInfo = new ProcessStartInfo
+{
+  FileName = "gcc",
+  UseShellExecute = false,
+  RedirectStandardError = true,
+  CreateNoWindow = true
+};
+
+startInfo.ArgumentList.Add("-E");
+startInfo.ArgumentList.Add("-P");
+startInfo.ArgumentList.Add(inputFile);
+startInfo.ArgumentList.Add("-o");
+startInfo.ArgumentList.Add(preprocessedFile);
+
 try
 {
-  var contents = File.ReadAllText(args[0], Encoding.UTF8);
-  Console.Write(contents);
-  return 0;
+  using var process = new Process { StartInfo = startInfo };
+  process.Start();
+
+  var diagnostics = process.StandardError.ReadToEnd();
+  process.WaitForExit();
+
+  Console.Error.Write(diagnostics);
+  return process.ExitCode;
 }
-catch (IOException)
+catch (Win32Exception)
 {
-  Console.Error.WriteLine($"Error: could not read source file '{args[0]}'.");
+  Console.Error.WriteLine("Error: could not start gcc. Ensure gcc is installed and available on PATH.");
   return 1;
 }
-catch (UnauthorizedAccessException)
+catch (InvalidOperationException)
 {
-  Console.Error.WriteLine($"Error: could not read source file '{args[0]}'.");
+  Console.Error.WriteLine("Error: could not start gcc.");
   return 1;
 }

eeb599180e123760e51de2218ed6425166611393
Zach Dillard <zachdillard@users.noreply.github.com>
2026-09-03 10:27:25 -0400

    Implement ParseFunction for parser concept

--- concepts/parser.cs
+++ concepts/parser.cs
@@ -1,4 +1,5 @@
 List<string> tokens = ["int", "main", "(", "void", ")", "{", "return", "2", ";", "}"];
+List<string> keywords = ["int", "void", "return"];
 
 var program = ParseProgram(tokens);
 
@@ -9,20 +10,38 @@ ProgramNode ParseProgram(List<string> tokens)
 
 FunctionNode ParseFunction(List<string> tokens)
 {
-    return null;
+    Expect("int", tokens);
+
+    string identifier = TakeToken(tokens);
+    if (keywords.Contains(identifier))
+        throw new InvalidOperationException();
+
+    Expect("(", tokens);
+    Expect("void", tokens);
+    Expect(")", tokens);
+    Expect("{", tokens);
+    StatementNode statement = ParseStatement(tokens);
+    Expect("}", tokens);
+
+    return new FunctionNode(identifier, statement);
 }
 
 StatementNode ParseStatement(List<string> tokens)
 {
     Expect("return", tokens);
-    ExpressionNode returnValue = ParseExpression(tokens);
+    ExpressionNode expression = ParseExpression(tokens);
     Expect(";", tokens);
-    return new StatementNode(returnValue);
+
+    return new ReturnStatementNode(expression);
 }
 
 ExpressionNode ParseExpression(List<string> tokens)
 {
-    return new ExpressionNode(0);
+    string token = TakeToken(tokens);
+    if (int.TryParse(token, out int constant) == false)
+        throw new InvalidOperationException();
+
+    return new ConstantExpressionNode(constant);
 }
 
 void Expect(string expected, List<string> tokens)
@@ -40,8 +59,8 @@ string TakeToken(List<string> tokens)
 }
 
 abstract record AST;
-record ProgramNode(FunctionNode FunctionDefinition) : AST;
-record FunctionNode(string Name, StatementNode Body) : AST;
+record ProgramNode(FunctionNode Function) : AST;
+record FunctionNode(string Identifier, StatementNode Statement) : AST;
 record StatementNode() : AST;
 record ReturnStatementNode(ExpressionNode Expression) : StatementNode;
 record ExpressionNode() : AST;
